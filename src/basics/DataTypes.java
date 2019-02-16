f3dc4e039e15deb9e41d5052ff28cf19f659b420
Mandula Naresh Babu <naresh2223@gmail.com>
2019-02-15 21:22:42 -0500

    15-Feb-2019: Naresh: Added DataTypes class file

---
+++ src/basics/DataTypes.java
@@ -0,0 +1,13 @@
+package basics;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		
+		// Primitive and Non Primitive Data Types
+		
+		
+
+	}
+
+}
--- src/basics/Shortcuts.java
+++ src/basics/Shortcuts.java
@@ -4,6 +4,8 @@ public class Shortcuts {
 
 	public static void main(String[] args) {
 		System.out.println("Use // to comment a line");
+		System.out.println("To comment block of code  use - Ctrl+Shift+/");
+		System.out.println("To uncomment block of code  use - Ctrl+Shift+\\");
 	}
 
 }
