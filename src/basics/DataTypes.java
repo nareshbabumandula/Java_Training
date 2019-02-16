c394ab9fb4e4a7f57f6868a1213ab8a08c3041c6
Mandula Naresh Babu <naresh2223@gmail.com>
2019-02-15 22:08:11 -0500

    15-Feb-2019: Naresh: Udpated DataTypes file

--- src/basics/DataTypes.java
+++ src/basics/DataTypes.java
@@ -6,7 +6,23 @@ public class DataTypes {
 		
 		// Primitive and Non Primitive Data Types
 		
+		int a=10;
+		float b=12.43f;
+		double c= 123.2342343;
+		char d = 'e';
+		boolean e=true;
+		boolean f=false;
+		long l=12435336;
+		short s=32100;
 		
+		System.out.println("Integer value of a is : " + a); // Integer 
+		System.out.println("Float value of b is : " + b); // Float
+		System.out.println("Double value of c is : " + c); // Double
+		System.out.println("Char value of d is : " + d); // Char
+		System.out.println("Boolean value of e is : " + e); // Boolean
+		System.out.println("Long value of l is : " + l); // Long
+		System.out.println("Short value of s is : " + s); // Short
+		System.out.println(a + " " + b + " " +c); // Print all
 
 	}
 
