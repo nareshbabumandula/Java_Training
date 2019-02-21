6305238238d3b5ee0324a842850ad63717009647
Mandula Naresh Babu <naresh2223@gmail.com>
2019-02-20 21:49:06 -0500

    20-Feb-2019:Naresh:Updated DataTypes

--- src/basics/DataTypes.java
+++ src/basics/DataTypes.java
@@ -5,16 +5,21 @@ public class DataTypes {
 	public static void main(String[] args) {
 		
 		// Primitive and Non Primitive Data Types
-		
 		int a=10;
 		float b=12.43f;
 		double c= 123.2342343;
 		char d = 'e';
 		boolean e=true;
+		
 		boolean f=false;
 		long l=12435336;
 		short s=32100;
+		byte b1 = 127;
 		
+		//Narrowing and widening
+		byte b2 = (byte) 130;
+		
+		System.out.println("Byte value is : " + b2);
 		System.out.println("Integer value of a is : " + a); // Integer 
 		System.out.println("Float value of b is : " + b); // Float
 		System.out.println("Double value of c is : " + c); // Double
@@ -23,7 +28,12 @@ public class DataTypes {
 		System.out.println("Long value of l is : " + l); // Long
 		System.out.println("Short value of s is : " + s); // Short
 		System.out.println(a + " " + b + " " +c); // Print all
+		
+		String sText = "hello world";
 
+		
+		
+		
 	}
 
 }
