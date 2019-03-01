04e0f96550c266b18b937ba0318c71e23721d145
UVR <47681388@users.noreply.github.com>
2019-02-28 22:27:14 -0500

    28-Feb-19: Usha: Updated shortcut for breakpoint in Shortcuts.Java file

--- src/basics/Shortcuts.java
+++ src/basics/Shortcuts.java
@@ -5,6 +5,7 @@ public class Shortcuts {
 	public static void main(String[] args) {
 		System.out.println("Use // to comment a line");
 		System.out.println("To comment block of code  use - Ctrl+Shift+/");
+		System.out.println("To put a breakpoint in code use CTrl+Shift+B");
 		System.out.println("To uncomment block of code  use - Ctrl+Shift+\\");
 	}
 
